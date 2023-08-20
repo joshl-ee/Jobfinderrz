62cbe91dfe2796953d6aab945648174318b50b94
joshl-ee <joshlee@usc.edu>
2023-08-19 18:28:21 -0700

    Delete client directory

--- client/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders phone number label', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Phone Number/i);
-  expect(linkElement).toBeInTheDocument();
-});
